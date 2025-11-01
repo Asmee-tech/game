54e64fc7bd22dcd4af0454ecd4b7117dfaba21a9
Asmee-tech <asmee.khimani@gmail.com>
2025-11-01 10:39:58 +0100

    Enhance game with text and mouse interactions
    Added text rendering and mouse event handling.

--- game.py
+++ game.py
@@ -3,18 +3,30 @@ pygame.init()
 width=600
 height=600
 screen=pygame.display.set_mode((width,height))
+screen.fill("black")
 img1=pygame.image.load("amongus.png")
 img2=pygame.image.load("clash.jpeg")
 img3=pygame.image.load("mario.png")
 img4=pygame.image.load("minecraft.png")
 
-img1=pygame.transform.scale(img1,(120,120))
+img1=pygame.transform.scale(img1,(120,110))
 img2=pygame.transform.scale(img2,(120,120))
 img3=pygame.transform.scale(img3,(120,120))
 img4=pygame.transform.scale(img4,(120,120))
-
-
-
+#Font and text
+fon=pygame.font.SysFont("arial",80)
+text1=fon.render("Mario",True,"white")
+screen.blit(text1,(280,20))
+text2=fon.render("Minecraft",True,"white")
+screen.blit(text2,(220,180))
+text3=fon.render("Among Us",True,"white")
+screen.blit(text3,(200,330))
+text4=fon.render("Clash",True,"white")
+screen.blit(text4,(300,470))
+screen.blit(img1,(20,10))
+screen.blit(img2,(20,150))
+screen.blit(img3,(20,290))
+screen.blit(img4,(20,460))
 
 
 
@@ -23,13 +35,21 @@ while run:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             run=False
-    screen.blit(img1,(20,10))
-    screen.blit(img2,(20,150))
-    screen.blit(img3,(20,290))
-    screen.blit(img4,(20,460))
 
-    #text1=font.render("Trust me, I’m not the Impostor.",True,"white")
-    #screen.blit(text1,(30,100))
+    #event trigger
+    event=pygame.event.poll()
+    if event.type==pygame.MOUSEBUTTONDOWN:
+        pos=pygame.mouse.get_pos()
+        pygame.draw.circle(screen,(100,200,155),(pos),25,25)
+        pygame.display.update()
+    elif event.type==pygame.MOUSEBUTTONUP:
+        pos2=pygame.mouse.get_pos()
+        pygame.draw.line(screen,(100,200,155),(pos),(pos2),5)
+        pygame.draw.circle(screen,(100,200,155),(pos2),25,25)
+        pygame.display.update()
+        
+        
+    
             
 
             
